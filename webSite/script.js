a6f83d1e086e8aeb8266b12815ed11fd06d8dc1a
Berthier Théo <theo.berthier@orange.com>
2024-01-22 17:51:45 +0100

    Add azure functions and update structure of project for deployment

--- webSite/script.js
+++ webSite/script.js
@@ -1,20 +1,29 @@
 document.querySelector("html").classList.add('js');
 
-var fileInput  = document.querySelector( ".input-file" ),  
-    button     = document.querySelector( ".input-file-trigger" ),
+var fileInput = document.querySelector(".input-file"),
+    button = document.querySelector(".input-file-trigger"),
     the_return = document.querySelector(".file-return");
-    
 
-button.addEventListener( "keydown", function( event ) {  
-    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
-        fileInput.focus();  
-    }  
+
+button.addEventListener("keydown", function (event) {
+    if (event.keyCode == 13 || event.keyCode == 32) {
+        fileInput.focus();
+    }
 });
-button.addEventListener( "click", function( event ) {
-   fileInput.focus();
-   return false;
-});  
-fileInput.addEventListener( "change", function( event ) {  
+button.addEventListener("click", function (event) {
+    fileInput.focus();
+    return false;
+});
+fileInput.addEventListener("change", function (event) {
     the_return.innerHTML = this.value;
-    console.log(fileInput.files[0]);
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        console.log('Contenu de l\'image en base64:', e.target.result);
+
+        // Par exemple, pour afficher l'image dans une balise img
+        let imageElement = document.createElement('img');
+        imageElement.src = e.target.result;
+        document.body.appendChild(imageElement);
+    }
+    reader.readAsDataURL(this.files[0]);
 });  
