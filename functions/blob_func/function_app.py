5f6ca717d0a73064204d2cae6ca0aa4fd956e5cc
Berthier Théo <theo.berthier.auditeur@lecnam.net>
2024-02-14 14:56:36 +0100

    add variables

--- functions/blob_func/function_app.py
+++ functions/blob_func/function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 
 @app.blob_trigger(arg_name="myblob", path="blob-container/{name}.png",
-                               connection="DefaultEndpointsProtocol=https;AccountName=myresourcesgroupb3b7;AccountKey=LLb3BeU4v/CHedCO3QS3SglPMYygk5dnefSZJXrHqBm1JD30aFIUPG29ooAiQMZMOziP07H8m8KY+ASt/IWk0Q==;EndpointSuffix=core.windows.ne") 
+                               connection="AzureWebJobsStorage") 
 def BlobTrigger(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
